a3c2d033c648fbba0075c2bc57fdd7104896bd9a
code1218 <skjil1218@gmail.com>
2023-02-08 12:51:20 +0900

    서버구축 및 request, response 구현중

--- src/j25_소켓/multiSocket/client/Client.java
+++ src/j25_소켓/multiSocket/client/Client.java
@@ -6,12 +6,9 @@ import java.net.UnknownHostException;
 
 public class Client {
 	
-	public static String name;
-	
 	public static void main(String[] args) {
 		try {
 			Socket socket = new Socket("127.0.0.1", 9090);
-			System.out.println("서버에 접속함?");
 			
 			ClientRecive clientRecive = new ClientRecive(socket);
 			clientRecive.start();
